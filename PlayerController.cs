f952107e70816594362f7000ab0677a3c74eb22f
jlsgkid <lijinliang@lijinliangdeMacBook-Pro.local>
2017-02-05 20:41:21 +0900

    Roll a boll

---
+++ PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public GameObject pick;
+	private int score;
+	public  Text count;
+
+	private float sp = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		showSocre ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void FixedUpdate(){
+		
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		GetComponent<Rigidbody> ().AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider collider){
+
+		if(collider.gameObject.tag == "PickUp"){
+			collider.gameObject.SetActive (false);
+			//Destroy ();
+		}
+
+		GameObject obj = Instantiate (pick) as GameObject;
+		obj.name = "PickUp" + Random.Range(10,100);
+		obj.transform.position = new Vector3 (Random.Range (-8.0f, 8.0f), 0.0f, Random.Range (-8.0f, 8.0f));
+		obj.gameObject.SetActive (true);
+	
+		score+=100;
+		showSocre ();
+	}
+
+	void showSocre(){
+		count.text = "score: " + score;
+	}
+	void Up(){
+
+		transform.Translate(Vector3.forward * Time.deltaTime* sp);  
+		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		//GetComponent<Rigidbody> ().AddForce (movement * speed * Time.deltaTime);
+	}
+	void Down(){
+		transform.Translate(-Vector3.forward * Time.deltaTime * sp);  
+	}
+	void Left(){
+		transform.Translate(Vector3.left * Time.deltaTime* sp);  
+	}
+	void Right(){
+		transform.Translate(Vector3.right * Time.deltaTime* sp);  
+	}
+}
---
+++ cameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+	public GameObject ball;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		//this.GetComponent<Transform>().position
+		transform.position = ball.transform.position + offset;
+	}
+
+}
